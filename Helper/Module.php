bb513ec0722032a4bfd63791c9a5a0ccbb04b9af
Sergey Poprygin <sergei.p@ecomitize.com>
2022-10-11 09:47:08 +0300

    PUN-98

---
+++ Helper/Module.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Punchout2Go\PurchaseOrder\Helper;
+
+use TradeCentric\Version\Helper\ModuleHelper;
+
+/**
+ * Class Module
+ * @package Punchout2Go\PurchaseOrder\Helper
+ */
+class Module extends ModuleHelper
+{
+
+}
---
+++ Model/Transfer/QuoteDataHandlers/Version.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace Punchout2Go\PurchaseOrder\Model\Transfer\QuoteDataHandlers;
+
+use Punchout2Go\Punchout\Model\Transfer\QuoteDataHandlerInterface;
+use TradeCentric\Version\Api\ModuleHelperInterface;
+
+/**
+ * Class Version
+ * @package TradeCentric\PurchaseOrder\Model\Transfer\QuoteDataHandlers
+ */
+class Version implements QuoteDataHandlerInterface
+{
+    /**
+     * @var ModuleHelperInterface
+     */
+    protected $helper;
+
+    /**
+     * @param ModuleHelperInterface $helper
+     */
+    public function __construct(ModuleHelperInterface $helper)
+    {
+        $this->helper = $helper;
+    }
+
+    /**
+     * @param \Magento\Quote\Api\Data\CartInterface $cart
+     * @return array
+     */
+    public function handle(\Magento\Quote\Api\Data\CartInterface $cart): array
+    {
+        return [
+            'custom_fields' => [
+                [
+                    'field' => 'purchase_order_extension',
+                    'value' => $this->helper->getModuleVersion()
+                ]
+            ]
+        ];
+    }
+}
