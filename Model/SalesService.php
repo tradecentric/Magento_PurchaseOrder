f0e20293a32b0e3ede59c5ba04c74e9e647345f8
dan bosler <dan.bosler@tradecentric.com>
2025-02-07 16:03:51 -0500

    CN-370_signe_cart

--- Model/SalesService.php
+++ Model/SalesService.php
@@ -305,20 +305,15 @@ class SalesService implements SalesServiceInterface
             }
             $isItem = $quote->getItemByProduct($product);
             if ($isItem) {
-	$this->logger->info("isItem - item->getItemId(): " . $item->getItemId());
-	$this->logger->info("isItem - product->getId: " . $product->getId());
-	$this->logger->info("isItem - product->getItemId: " . $product->getTypeId());
-	
-//	$selectionIds = $product->getCustomOption('bundle_selection_ids');
-//	$selectionIds = $this->serializer->unserialize($selectionIds->getValue());
-//	$this->logger->info("isItem - product->getItemId: " . $product->getCustomOption('bundle_selection_ids'));
-	
                 $quoteItem = $this->quoteItemFactory->create();
                 $quoteItem->setQty($item->getQty());
                 $quoteItem->setPrice($product->getPrice());
                 $quoteItem->setProductType($product->getTypeId());
-                $quoteItem->setOriginalPrice($product->getPrice());
-                $quoteItem->setProduct($product);
+                $quoteItem->setOriginalPrice($product->getPrice());        
+				if ($product->getTypeId() == 'bundle' ) {
+					$QuoteItem->setCustomOption($product->getCustomOption('bundle_selection_ids'));		
+				}
+				$quoteItem->setProduct($product);
                 $quote->addItem($quoteItem);
             } else {
                 $quoteItem = $quote->addProduct($product, $item->getQty());
