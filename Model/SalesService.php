932f69a72fe57237258c06c56a09c6eaae3c32c6
dan bosler <dan.bosler@tradecentric.com>
2025-02-11 09:47:26 -0500

    CN-370 update

--- Model/SalesService.php
+++ Model/SalesService.php
@@ -324,7 +324,6 @@ class SalesService implements SalesServiceInterface
 					foreach ($childQuoteItems as $child) {
 	$this->logger->info("get child->getProductId: " . $child->getProductId());
 	$this->logger->info("get child->getPrice: " . $child->getPrice());
-	/*
 						$childItem->setProduct($child->getProduct());	
 						$childItem = $this->quoteItemFactory->create();
 						$childItem->setQuoteId($item->getQuoteId());
@@ -338,7 +337,6 @@ class SalesService implements SalesServiceInterface
 						$childItem->setProductType($child->getTypeId());
 						$childItem->setOriginalPrice($child->getPrice());
 						$quote->addItem($childItem);
-	*/
 					}
 				}
             } else {
